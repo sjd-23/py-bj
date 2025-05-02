cc9a0ec3a3bdae50f176f1fcdb6043329b2bead7
sjd23dev <SJD23Dev@proton.me>
2025-05-01 21:32:58 -0500

    Update model to add the deck, player, and dealer, as well as updating in update

--- core/model.py
+++ core/model.py
@@ -1,6 +1,16 @@
+from game.deck import Deck
+from game.player import Player
+
 class Model:
     def __init__(self):
-        pass
+        self.deck = Deck()
+        self.player = Player()
+        self.dealer = Player()
+        self.deck.shuffle()
 
     def update(self, dt):
-        print("Delta time: " + str(dt))
+        for card in self.player.hand:
+            card.update(dt)
+
+        for card in self.dealer.hand:
+            card.update(dt)
