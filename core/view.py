47c710553f2b2712f6d15283211e0b8d34bae15c
sjd23dev <SJD23Dev@proton.me>
2025-05-01 21:32:26 -0500

    Update view to include user interface and ui drawing, also add green background drawing

--- core/view.py
+++ core/view.py
@@ -1,8 +1,27 @@
 import pygame
+from ui.hand_ui import HandUI
+from ui.dealer_ui import DealerUI
+from ui.hit_ui import HitUI
+from ui.stay_ui import StayUI
+from ui.game_over_ui import GameOverUI
 
 class View:
-    def __init__(self, screen):
+    def __init__(self, screen, model, font):
         self.screen = screen
+        self.model = model
+        self.hand_ui = HandUI(self.model.player, font)
+        self.dealer_ui = DealerUI(self.model.dealer, font)
+        self.hit_ui = HitUI(self.model.player, font)
+        self.stay_ui = StayUI(self.model.dealer, font)
+        self.game_over_ui = GameOverUI(self.model.player, self.model.dealer, font)
 
     def draw(self):
+        pygame.draw.rect(self.screen, "dark green", (0, 0, 1280, 720))
+
+        self.hand_ui.draw(self.screen)
+        self.dealer_ui.draw(self.screen)
+        self.hit_ui.draw(self.screen)
+        self.stay_ui.draw(self.screen)
+        self.game_over_ui.draw(self.screen)
+
         pygame.display.flip()
